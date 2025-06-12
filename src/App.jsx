b583ae123715956bd59fe64b48876cfa642bd739
Sneh Doshi <doshisneh75@gmail.com>
2025-06-12 11:46:07 +0530

    Created the Hero Bar , Added The title and images into the webpage along with styling it

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import Header from "./components/Header"
+import Hero from "./components/Hero"
 
 
 
 const App =  () => {
   return (
+    <>
     <Header />
+    <main>
+      <Hero />
+    </main>
+     </>
   )
 }
 
