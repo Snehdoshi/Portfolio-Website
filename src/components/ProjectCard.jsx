f8424c67d7d44a13c09e0fec996ad3d849bae14d
Sneh Doshi <doshisneh75@gmail.com>
2025-06-18 20:22:20 +0530

    Created the Work Section and fixed all Bugs and Errors

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -9,8 +9,8 @@ const ProjectCard = ({
   classes
 }) => {
   return (
-    <div className="">
-      <figure className="img-box ">
+    <div className={"relative p-4 rounded-2xl bg-zinc-800 hover:bg-zinc-700/50  active:bg-zinc-700/60  ring-1 ring-inset  ring-zinc-50/5   transition-colors" + classes}>
+      <figure className="img-box aspect-square rounded-lg mb-4">
         <img 
         src={imgSrc} 
         alt={title}
@@ -19,21 +19,47 @@ const ProjectCard = ({
         />
       </figure>
 
-      <div className="">
+      <div className="flex items-center justify-between gap-4">
 
       <div> 
-        <h3 className="title-1">
+        <h3 className="title-1 mb-3">
           {title}
         </h3>
+
+        <div className="flex flex-wrap items-center gap-2">
+          {tags.map((label , key) => (
+            <span 
+            key={key}
+            className="h-8 text-sm text-zinc-400 bg-zinc-50/5 grid items-center px-3 rounded-lg"
+            >
+              {label}
+            </span>
+          ))}
+        </div>
+      </div>
+
+      <div className="w-11 h-11 rounded-lg grid place-items-center bg-sky-400 text-zinc-950 shrink-0">
+        <span 
+        aria-hidden = "true"
+        className="material-symbols-rounded">
+          arrow_outward
+        </span>
       </div>
 
       </div>
+
+          <a 
+          href={projectLink} 
+          className="absolute inset-0"
+          target='_blank'
+          ></a>
+
     </div>
     
   )
 }
 
-ProjectCard.PropTypes = {
+ProjectCard.propTypes = {
   imgSrc: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   tags: PropTypes.array.isRequired,
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -48,13 +48,19 @@ const Work = () => {
     className="section">
 
       <div className="container">
-        <h2 className="headline-2">
+        <h2 className="headline-2 mb-8">
           My Portfolio Highlights:
         </h2>
 
-        <div className="">
+        <div className="grid gap-x-4 gap-y-5 grid-cols-[repeat(auto-fill,minmax(280px,1fr))]">
           {works.map(({ imgSrc , title , tags , projectLink} , key) => (
-            <ProjectCard/>
+            <ProjectCard 
+              key={key}
+              imgSrc={imgSrc} 
+              title={title}
+              tags={tags} 
+              projectLink={projectLink}
+              />
           ))}
         </div>
       </div>
